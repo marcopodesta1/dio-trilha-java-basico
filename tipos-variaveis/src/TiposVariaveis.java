be5fbf5ada97065ebf0c95f0be5fa2c3dc2b4128
marcopodesta1 <marcopodesta@hotmail.com>
2025-03-24 21:53:43 -0300

    commit após estudo de tipos primitivos e um pouco de String

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
+
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        //Estudem a classe String que representa texto na aplicação
+
+        String meuNome = "MARCO AURELIO ";
+        var teste = meuNome + " tem varias letras";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(teste);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
