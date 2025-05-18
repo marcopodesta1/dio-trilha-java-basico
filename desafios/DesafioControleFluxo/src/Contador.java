11e420ef060c65e0e6fc1701c0625fc344f38026
marcopodesta1 <marcopodesta@hotmail.com>
2025-05-17 21:22:33 -0300

    commit exercício área figura geométrica

--- desafios/DesafioControleFluxo/src/Contador.java
+++ desafios/DesafioControleFluxo/src/Contador.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.print("Digite o primeiro parâmetro: ");
-        int parametroUm = terminal.nextInt();
+         int parametroUm = terminal.nextInt();
         System.out.print("Digite o segundo parâmetro: ");
         int parametroDois = terminal.nextInt();
         terminal.close();
